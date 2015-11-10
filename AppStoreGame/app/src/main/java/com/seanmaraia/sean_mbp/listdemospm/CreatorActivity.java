9dae4568e320b4da32d7fd9c467768bf3b7841cb
Haplicity <fu_charles@yahoo.com>
2015-11-10 15:18:23 -0500

    CreatorActivity now passes data back to ListActivity

--- AppStoreGame/app/src/main/java/com/seanmaraia/sean_mbp/listdemospm/CreatorActivity.java
+++ AppStoreGame/app/src/main/java/com/seanmaraia/sean_mbp/listdemospm/CreatorActivity.java
@@ -1,15 +1,18 @@
 package com.seanmaraia.sean_mbp.listdemospm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class CreatorActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner themeSpinner, typeSpinner, styleSpinner;
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,30 +21,34 @@ public class CreatorActivity extends AppCompatActivity implements AdapterView.On
 
         themeSpinner = (Spinner) findViewById(R.id.theme_spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.theme_spinner_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.theme_spinner_array, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         themeSpinner.setAdapter(adapter);
 
         typeSpinner = (Spinner) findViewById(R.id.type_spinner);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
-                R.array.type_spinner_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.type_spinner_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
         typeSpinner.setAdapter(adapter2);
 
         styleSpinner = (Spinner) findViewById(R.id.style_spinner);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
-                R.array.theme_spinner_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.style_spinner_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
         styleSpinner.setAdapter(adapter3);
+
+        submitButton = (Button)findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreatorActivity.this, ListActivity.class);
+                intent.putExtra(ListActivity.THEME_DATA, themeSpinner.getSelectedItem().toString());
+                intent.putExtra(ListActivity.TYPE_DATA, typeSpinner.getSelectedItem().toString());
+                intent.putExtra(ListActivity.STYLE_DATA, styleSpinner.getSelectedItem().toString());
+                setResult(ListActivity.RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
--- AppStoreGame/app/src/main/java/com/seanmaraia/sean_mbp/listdemospm/ListActivity.java
+++ AppStoreGame/app/src/main/java/com/seanmaraia/sean_mbp/listdemospm/ListActivity.java
@@ -1,12 +1,12 @@
 package com.seanmaraia.sean_mbp.listdemospm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
-<<<<<<< HEAD
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
@@ -14,38 +14,39 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.view.View;
 import android.widget.RelativeLayout;
-
 import java.text.SimpleDateFormat;
-=======
-
->>>>>>> 3b56c04ecccf62bbd32a4469ad39fceed641ee71
 import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
 
-<<<<<<< HEAD
+    static final int CREATOR_ACTIVITY_REQUEST = 1;
+    public final static String THEME_DATA = "THEME";
+    public final static String TYPE_DATA = "TYPE";
+    public final static String STYLE_DATA = "STYLE";
+
     Button testcreateButton;
-=======
+
     RecyclerView mRecyclerView;
     AppItemAdapter mAdapter;
     ArrayList<AppItem> mData;
->>>>>>> 3b56c04ecccf62bbd32a4469ad39fceed641ee71
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_activity);
 
-<<<<<<< HEAD
         testcreateButton = (Button)findViewById(R.id.testcreate_button);
         testcreateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Intent intent = new Intent(ListActivity.this, CreatorActivity.class);
+                //startActivity(intent);
+
                 Intent intent = new Intent(ListActivity.this, CreatorActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, CREATOR_ACTIVITY_REQUEST);
             }
         });
-=======
+
         DataStore dataStore = DataStore.get(this);
         mData = dataStore.getData();
 
@@ -56,7 +57,6 @@ public class ListActivity extends AppCompatActivity {
         mAdapter = new AppItemAdapter(mData);
         mRecyclerView.setAdapter(mAdapter);
 
->>>>>>> 3b56c04ecccf62bbd32a4469ad39fceed641ee71
     }
 
     @Override
@@ -67,5 +67,19 @@ public class ListActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == CREATOR_ACTIVITY_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // Retrieve the data
+                String themeString = data.getStringExtra(THEME_DATA);
+                String typeString = data.getStringExtra(TYPE_DATA);
+                String styleString = data.getStringExtra(STYLE_DATA);
+            }
+        }
     }
 }
